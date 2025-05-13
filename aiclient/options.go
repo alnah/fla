4f83b85453459e446963f65a611b427ae3bdeaca
alnah <alexis.nahan@gmail.com>
2025-05-13 08:06:11 -0300

    feat(aiclient): add options

---
+++ aiclient/options.go
@@ -0,0 +1,22 @@
+package aiclient
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/alnah/fla/logger"
+)
+
+type Option func(*ChatClient)
+
+func WithProvider(p Provider) Option        { return func(cc *ChatClient) { cc.provider = p } }
+func WithURL(u URL) Option                  { return func(cc *ChatClient) { cc.url = u } }
+func WithAPIKey(a APIKey) Option            { return func(cc *ChatClient) { cc.apiKey = a } }
+func WithModel(a AIModel) Option            { return func(cc *ChatClient) { cc.Model = a } }
+func WithTemperature(t Temperature) Option  { return func(cc *ChatClient) { cc.Temperature = t } }
+func WithMessages(ms Messages) Option       { return func(cc *ChatClient) { cc.Messages = ms } }
+func WithMaxTokens(mt MaxTokens) Option     { return func(cc *ChatClient) { cc.MaxTokens = mt } }
+func WithContext(c context.Context) Option  { return func(cc *ChatClient) { cc.ctx = c } }
+func WithHTTPClient(hc *http.Client) Option { return func(cc *ChatClient) { cc.hc = hc } }
+func WithLogger(l *logger.Logger) Option    { return func(cc *ChatClient) { cc.logger = l } }
+func WithHTTPMethod(hm HTTPMethod) Option   { return func(cc *ChatClient) { cc.httpMethod = hm } }
