b6f86ee0c56a1b4eda7ea4647474be17516ac5c9
alnah <alexis.nahan@gmail.com>
2025-05-13 23:55:25 -0300

    style(aiclient): reorder options

--- aiclient/options.go
+++ aiclient/options.go
@@ -9,6 +9,7 @@ import (
 
 type Option func(*ChatClient)
 
+func WithContext(c context.Context) Option  { return func(cc *ChatClient) { cc.ctx = c } }
 func WithProvider(p Provider) Option        { return func(cc *ChatClient) { cc.provider = p } }
 func WithURL(u URL) Option                  { return func(cc *ChatClient) { cc.url = u } }
 func WithAPIKey(a APIKey) Option            { return func(cc *ChatClient) { cc.apiKey = a } }
@@ -16,7 +17,6 @@ func WithModel(a AIModel) Option            { return func(cc *ChatClient) { cc.M
 func WithTemperature(t Temperature) Option  { return func(cc *ChatClient) { cc.Temperature = t } }
 func WithMessages(ms Messages) Option       { return func(cc *ChatClient) { cc.Messages = ms } }
 func WithMaxTokens(mt MaxTokens) Option     { return func(cc *ChatClient) { cc.MaxTokens = mt } }
-func WithContext(c context.Context) Option  { return func(cc *ChatClient) { cc.ctx = c } }
 func WithHTTPClient(hc *http.Client) Option { return func(cc *ChatClient) { cc.httpClient = hc } }
-func WithLogger(l *logger.Logger) Option    { return func(cc *ChatClient) { cc.logger = l } }
 func WithHTTPMethod(hm HTTPMethod) Option   { return func(cc *ChatClient) { cc.httpMethod = hm } }
+func WithLogger(l *logger.Logger) Option    { return func(cc *ChatClient) { cc.logger = l } }
