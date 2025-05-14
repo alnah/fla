010e5cc33a2736bcb60bd8719604b0cb7241fb37
alnah <alexis.nahan@gmail.com>
2025-05-14 11:20:41 -0300

    refactor(aiclient): update http error client error string

--- aiclient/http_error.go
+++ aiclient/http_error.go
@@ -1,6 +1,9 @@
 package aiclient
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type HTTPClientError struct {
 	Provider  Provider
@@ -9,13 +12,59 @@ type HTTPClientError struct {
 	Wrapped   error
 }
 
-func (e *HTTPClientError) Error() string {
-	if e.Wrapped != nil {
-		return fmt.Sprintf("%s %s: %s: %v", e.Operation.String(), e.Provider.String(), e.Message, e.Wrapped)
+func (e HTTPClientError) Error() string {
+	const prefix = "http client error"
+
+	// build the "core" piece: operation and/or provider
+	var core string
+	if e.Operation != "" {
+		core = fmt.Sprintf("operation %s failed", e.Operation)
+		if e.Provider != "" {
+			core += fmt.Sprintf(" for %s", e.Provider)
+		}
+	} else if e.Provider != "" {
+		core = fmt.Sprintf("for %s", e.Provider)
 	}
-	return fmt.Sprintf("%s %s: %s", e.Operation, e.Provider, e.Message)
+
+	// collect remaining pieces: message and wrapped error
+	parts := []string{}
+	if core != "" {
+		parts = append(parts, core)
+	}
+	if e.Message != "" {
+		parts = append(parts, e.Message)
+	}
+	if e.Wrapped != nil {
+		parts = append(parts, e.Wrapped.Error())
+	}
+
+	// assemble final string
+	if len(parts) == 0 {
+		return prefix
+	}
+
+	// choose separator before the first part
+	first := parts[0]
+	var sep string
+	switch {
+	case strings.HasPrefix(first, "operation"):
+		sep = ": "
+	case strings.HasPrefix(first, "for"):
+		sep = " "
+	default:
+		sep = ": "
+	}
+
+	result := prefix + sep + first
+	for _, p := range parts[1:] {
+		result += ": " + p
+	}
+	return result
 }
-func (e *HTTPClientError) Unwrap() error { return e.Wrapped }
+
+func (e *HTTPClientError) Unwrap() error {
+	return e.Wrapped
+}
 
 func NewChatClientError(p Provider, m string, w error) *HTTPClientError {
 	return &HTTPClientError{
