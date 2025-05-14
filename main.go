7fe22aaca317d484cf34cb622ee51595b55cf27e
alnah <alexis.nahan@gmail.com>
2025-05-14 07:28:35 -0300

    refactor(main): update demo

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
-	"flag"
+	"os"
 
-	ai "github.com/alnah/fla/aiclient"
+	"github.com/alnah/fla/cli"
 	"github.com/alnah/fla/config"
 	"github.com/alnah/fla/logger"
 )
@@ -19,15 +19,9 @@ func main() {
 		log.Error("retrieving config path", "error", err.Error())
 		return
 	}
-	cfg, err := config.Load(log, path)
+	cfg, err := config.Load(log, path.DirPath, path.FileName)
 	if err != nil {
 		log.Error("loading config", "error", err.Error())
-		return
-	}
-	cfg.BindFlags()
-	flag.Parse()
-	if err := cfg.Validate(); err != nil {
-		log.Error("validating config", "error", err.Error())
 		return
 	}
 	if err := cfg.EnsureIODirs(); err != nil {
@@ -35,42 +29,8 @@ func main() {
 		return
 	}
 
-	/********* Log config *********/
-	cfg.Log.Info("config",
-		"language", cfg.Language,
-		"input", cfg.Input,
-		"output", cfg.Output,
-		"ai_completion_timeout", cfg.AI.Timeout.Completion,
-		"ai_audio_timeout", cfg.AI.Timeout.Audio,
-		"ai_transcription_timeout", cfg.AI.Timeout.Transcription,
-	)
-
-	/******** Demo *********/
-	ctx, cancel := context.WithTimeout(context.Background(), cfg.AI.Timeout.Completion)
-	defer cancel()
-
-	chat, err := ai.NewChatClient(
-		ai.WithProvider(ai.ProviderAnthropic),
-		ai.WithURL(ai.URLChatCompletionAnthropic),
-		ai.WithAPIKey(ai.EnvAnthropicAPIKey),
-		ai.WithModel(ai.AIModelCostOptimizedAnthropic),
-		ai.WithContext(ctx),
-		ai.WithLogger(log),
-		ai.WithMessages(ai.Messages{
-			ai.Message{
-				Role:    ai.RoleUser,
-				Content: "Comment vas-tu ?",
-			},
-		}),
-	)
-	if err != nil {
-		log.Error("new chat client failed", "error", err.Error())
-		return
+	cmd := cli.NewFlaCmd()
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Error("root command", "error", err.Error())
 	}
-	completion, err := chat.Do()
-	if err != nil {
-		log.Error("completion failed", "error", err.Error())
-		return
-	}
-	log.Info("completion succeed", "content", completion.String())
 }
