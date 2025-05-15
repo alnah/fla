8185f47a4acfb171dec9b27bcd545ed015228acc
alnah <alexis.nahan@gmail.com>
2025-05-14 23:38:29 -0300

    feat(aiclient): add transport chain to tts client, add doer

--- aiclient/tts.go
+++ aiclient/tts.go
@@ -1,8 +1,11 @@
 package aiclient
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 	"time"
@@ -31,6 +34,7 @@ func NewTTSClient(options ...Option[*TTSClient]) (*TTSClient, error) {
 	if err := t.applyDefaults().setProviderFlag().validate(); err != nil {
 		return nil, fmt.Errorf("failed to build tts client: %w", err)
 	}
+	t.base.httpClient.Transport = t.newTransportChain()
 	return t, nil
 }
 
@@ -54,4 +58,38 @@ func (t *TTSClient) setProviderFlag() *TTSClient {
 	t.UseOpenAI = strings.Contains(t.base.url.String(), ProviderOpenAI.String())
 	t.UseElevenLabs = strings.Contains(t.base.url.String(), ProviderElevenLabs.String())
 	return t
+}
+
+func (t *TTSClient) Do() ([]byte, error) {
+	byt, err := json.Marshal(t)
+	if err != nil {
+		return nil, NewTTSClientError(t.base.provider, "failed to marshal payload", err)
+	}
+
+	url := t.base.url.String()
+	if t.base.provider == ProviderElevenLabs {
+		url += "/" + t.Voice.String()
+	}
+	req, err := http.NewRequestWithContext(t.base.ctx, t.base.httpMethod.String(), url, bytes.NewBuffer(byt))
+	if err != nil {
+		return nil, NewTTSClientError(t.base.provider, "failed to build http request", err)
+	}
+
+	res, err := t.base.httpClient.Do(req)
+	if err != nil {
+		return nil, NewTTSClientError(t.base.provider, "failed to send http request", err)
+	}
+	defer func() { _ = res.Body.Close() }()
+
+	byt, err = io.ReadAll(res.Body)
+	res.Body = io.NopCloser(bytes.NewReader(byt))
+	if err != nil {
+		return nil, NewTTSClientError(t.base.provider, "failed to read response body", err)
+	}
+
+	if res.StatusCode != 200 {
+		return nil, BuildProviderError(t.base.provider, res)
+	}
+
+	return byt, nil
 }
