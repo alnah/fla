54fc710991e4b59e0da9287b1a9c2b7c66cddfa9
alnah <alexis.nahan@gmail.com>
2025-05-15 07:44:02 -0300

    style(aiclient): reorder options

--- aiclient/base_options.go
+++ aiclient/base_options.go
@@ -10,17 +10,11 @@ import (
 func WithContext[T hasBase](c context.Context) Option[T] {
 	return func(t T) { (t).BaseClient().ctx = c }
 }
-func WithProvider[T hasBase](p Provider) Option[T] {
-	return func(t T) { (t).BaseClient().provider = p }
+func WithLogger[T hasBase](l *logger.Logger) Option[T] {
+	return func(t T) { (t).BaseClient().logger = l }
 }
 func WithURL[T hasBase](u URL) Option[T] {
 	return func(t T) { (t).BaseClient().url = u }
-}
-func WithAPIKey[T hasBase](a APIKey) Option[T] {
-	return func(t T) { (t).BaseClient().apiKey = a }
-}
-func WithModel[T hasBase](a AIModel) Option[T] {
-	return func(t T) { (t).BaseClient().Model = a }
 }
 func WithHTTPClient[T hasBase](hc *http.Client) Option[T] {
 	return func(t T) { (t).BaseClient().httpClient = hc }
@@ -28,6 +22,12 @@ func WithHTTPClient[T hasBase](hc *http.Client) Option[T] {
 func WithHTTPMethod[T hasBase](hm HTTPMethod) Option[T] {
 	return func(t T) { (t).BaseClient().httpMethod = hm }
 }
-func WithLogger[T hasBase](l *logger.Logger) Option[T] {
-	return func(t T) { (t).BaseClient().logger = l }
+func WithAPIKey[T hasBase](a APIKey) Option[T] {
+	return func(t T) { (t).BaseClient().apiKey = a }
+}
+func WithProvider[T hasBase](p Provider) Option[T] {
+	return func(t T) { (t).BaseClient().provider = p }
+}
+func WithModel[T hasBase](a AIModel) Option[T] {
+	return func(t T) { (t).BaseClient().Model = a }
 }
