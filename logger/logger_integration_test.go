e2e389810abdb937ef11c42dec7a0c375d6088d7
alnah <alexis.nahan@gmail.com>
2025-05-17 14:29:46 -0300

    add integration test

---
+++ logger/logger_integration_test.go
@@ -0,0 +1,42 @@
+//go:build integration
+
+package logger
+
+import (
+	"bytes"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestLogger_WriteTo_Stdout(t *testing.T) {
+	testCases := []struct {
+		name string
+		fn   func() *Logger
+	}{
+		{name: "New", fn: New},
+		{name: "Test", fn: Test},
+	}
+	for _, tc := range testCases {
+		// save original stdout
+		origStdout := os.Stdout
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+
+		logger := tc.fn()
+		logger.Info("test", "foo", "bar")
+
+		// close the writer and restore stdout
+		w.Close()
+		os.Stdout = origStdout
+
+		var buf bytes.Buffer
+		_, _ = buf.ReadFrom(r)
+		output := buf.String()
+
+		if !strings.Contains(output, "msg=test") || !strings.Contains(output, "foo=bar") {
+			t.Errorf("didn't want log output: %s", output)
+		}
+
+	}
+}
--- logger/logger_test.go
+++ logger/logger_test.go
@@ -3,7 +3,6 @@ package logger
 import (
 	"bytes"
 	"log/slog"
-	"os"
 	"strings"
 	"testing"
 )
@@ -22,41 +21,5 @@ func TestLogger_Debug_Output(t *testing.T) {
 
 	if !strings.Contains(output, "msg=testing") || !strings.Contains(output, "key=value") {
 		t.Errorf("didn't want log output: %s", output)
-	}
-}
-
-func TestLogger_WriteTo_Stdout(t *testing.T) {
-	testCases := []struct {
-		name string
-		fn   func() *Logger
-	}{
-		{name: "New", fn: New},
-		{name: "Test", fn: Test},
-	}
-	for _, tc := range testCases {
-		if testing.Short() {
-			t.Skip("skipping integration test in short mode")
-		}
-
-		// save original stdout
-		origStdout := os.Stdout
-		r, w, _ := os.Pipe()
-		os.Stdout = w
-
-		logger := tc.fn()
-		logger.Info("test", "foo", "bar")
-
-		// close the writer and restore stdout
-		w.Close()
-		os.Stdout = origStdout
-
-		var buf bytes.Buffer
-		_, _ = buf.ReadFrom(r)
-		output := buf.String()
-
-		if !strings.Contains(output, "msg=test") || !strings.Contains(output, "foo=bar") {
-			t.Errorf("didn't want log output: %s", output)
-		}
-
 	}
 }
